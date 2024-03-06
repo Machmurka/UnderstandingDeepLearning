e3598f27deac5edc54f59ae6e1652fcfa6c8d2dc
jakmachura <jakmachura@gmail.com>
2024-03-06 22:47:40 +0100

    Learning abount tensor basics

--- Chapter8/TorchStuffJourney/Signle Hidden Unit Layer.py
+++ Chapter8/TorchStuffJourney/Signle Hidden Unit Layer.py
@@ -1,22 +1,28 @@
 #setting up torch 
 import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+# from torch import nn
+# from torch.utils.data import DataLoader
+# from torchvision import datasets
+# from torchvision.transforms import ToTensor
 
-# Download training data from open datasets.
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
+# print(torch.__version__)
 
-# Download test data from open datasets.
-test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor(),
-)
+scalar = torch.tensor(7)
+print(scalar)
+print("demention fo scalar (torch)",scalar.ndim)
+print()
+# # Download training data from open datasets.
+# training_data = datasets.FashionMNIST(
+#     root="data",
+#     train=True,
+#     download=True,
+#     transform=ToTensor(),
+# )
+
+# # Download test data from open datasets.
+# test_data = datasets.FashionMNIST(
+#     root="data",
+#     train=False,
+#     download=True,
+#     transform=ToTensor(),
+# )
