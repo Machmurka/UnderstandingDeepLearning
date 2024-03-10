312d8b82eb0c86178747d984b0baf677d078a355
jakmachura <jakmachura@gmail.com>
2024-03-10 19:29:07 +0100

    Refactor CreateData class and add loss function and training loop

--- Learn PyTorch for Deep Learning/01_pytorch_workflow_fundaments.py
+++ Learn PyTorch for Deep Learning/01_pytorch_workflow_fundaments.py
@@ -13,8 +13,9 @@ what_were_covering = {1: "data (prepare and load)",
 #Create Data for training model
 class CreateData:
     def __init__(self,bias,weigth) -> None:
+        self.bias , self.weigth = bias, weigth
         self.x=torch.arange(0,1,0.02).unsqueeze(dim=1) # usefull from [50] to [50,1] 
-        self.y=weigth*self.x+bias
+        self.y=self.weigth*self.x+self.bias
     
     #Create Train ^ Test slit 
     def CreateTrainTestSplit(self) ->None:
@@ -82,7 +83,8 @@ class LinearRegressionModel(nn.Module):
 
     def Optimizer(self):
         return torch.optim.SGD(params=self.parameters(),lr=0.01)
-
+    #loss function:
+    #nn.L1Loss()
 
 d=CreateData(0.3,0.7)
 d.CreateTrainTestSplit()
@@ -107,4 +109,67 @@ print(f"Number of testing samples: {len(d.x_test)}")
 print(f"Number of predictions made: {len(y_preds)}")
 print(f"Predicted values:\n{y_preds}")
 
-p.plot_predictions(d.GetX_train(),d.GetY_train(),d.GetX_test(),d.GetY_test(),predictions=y_preds)
+# p.plot_predictions(d.GetX_train(),d.GetY_train(),d.GetX_test(),d.GetY_test(),predictions=y_preds)
+
+loss_fn=nn.L1Loss()
+
+
+#set number of epochs
+epochs = 100
+
+#Create enpty loss list to track values
+train_loss_values=[]
+test_loss_values=[]
+epoch_count=[]
+
+#training
+for epoch in range(epochs):
+    # Put model in training mode (this is the default state of a model)
+    model0.train()
+
+    #1. forward pass on train data using forward()
+    y_pred=model0(d.x_train)
+
+    # 2. Calculate the loss
+    loss=loss_fn(y_pred,d.y_train)
+
+    # 3. Zero grad of the optimizer
+    model0.Optimizer().zero_grad()
+
+    #4. loss backwards
+    loss.backward()
+
+    #5. progress the optimizer
+    model0.Optimizer().step()
+
+    ##testing
+    model0.eval()
+
+    with torch.inference_mode():
+        # 1.forward pass on test data
+        test_pred=model0(d.x_test)
+
+        # 2. calculate loss on test data
+        test_loss = loss_fn(test_pred,d.y_test.type(torch.float))  # predictions come in torch.float datatype, so comparisons need to be done with tensors of the same type
+
+      # Print out what's happening
+        if epoch % 10 == 0:
+            epoch_count.append(epoch)
+            train_loss_values.append(loss.detach().numpy())
+            test_loss_values.append(test_loss.detach().numpy())
+            print(f"Epoch: {epoch} | MAE Train Loss: {loss} | MAE Test Loss: {test_loss} ")
+
+# Plot the loss curves
+plt.plot(epoch_count, train_loss_values, label="Train loss")
+plt.plot(epoch_count, test_loss_values, label="Test loss")
+plt.title("Training and test loss curves")
+plt.ylabel("Loss")
+plt.xlabel("Epochs")
+plt.legend()
+plt.show()
+
+# Find our model's learned parameters
+print("The model learned the following values for weights and bias:")
+print(model0.state_dict())
+print("\nAnd the original values for weights and bias are:")
+print(f"weights: {d.weigth}, bias: {d.bias}")
