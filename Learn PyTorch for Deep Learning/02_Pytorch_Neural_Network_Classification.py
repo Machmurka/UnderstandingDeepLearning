e94d9cce17f3bb3b21b17180da46413c62bcc6b6
jakmachura <jakmachura@gmail.com>
2024-03-12 12:44:04 +0100

    Add SingleLine class and CircleModelV2 class for training and plotting

--- Learn PyTorch for Deep Learning/02_Pytorch_Neural_Network_Classification.py
+++ Learn PyTorch for Deep Learning/02_Pytorch_Neural_Network_Classification.py
@@ -108,7 +108,12 @@ class PlotStuff():
             )
         plt.show()
 
-    
+    def PlotLine(self,model,predictions=None):
+        plt.scatter(model.x_train,model.y_train ,c='b' ,s=4)
+        plt.scatter(model.x_test,model.y_test , c='g',  s=4)
+        if predictions != None: 
+            plt.scatter(model.x_test,model.y_pred,c='r', s=4) 
+        plt.show()
     def PlotModel(self,model)->None:
         import requests
         from pathlib import Path 
@@ -185,7 +190,111 @@ class CircleModelV1(nn.Module):
                     print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
 
 
+class SingleLine():
+    def __init__(self) -> None:
+        self.CreateData()
 
+    def CreateData(self)->None:
+        self.x=torch.arange(0,1,0.01).unsqueeze(dim=1)
+        print(self.x)
+        self.y=0.3+0.7*self.x
+        self.Split()
+
+    def Split(self)->None:
+        split=int(len(self.x)*0.8)
+        self.x_train , self.y_train = self.x[:split] , self.y[:split]
+        self.x_test , self.y_test = self.x[split:] , self.y[split:]
+        # print(len(self.x_test))
+    def model(self)->None:
+        self.model_2= nn.Sequential(
+            nn.Linear(in_features=1 , out_features=10),
+            nn.Linear(in_features=10,out_features=10),
+            nn.Linear(in_features=10 , out_features=1)
+        )
+         
+        loss_fn=nn.L1Loss()
+        optimizer=torch.optim.SGD(self.model_2.parameters(),lr=0.1)
+
+        epochs=100
+        self.model_2.train()
+        for epoch in range(epochs):
+            # 1.forward pass
+            y_pred=self.model_2(self.x_train)
+            
+            # 2.calculate loss
+            loss = loss_fn(y_pred,self.y_train)
+
+            # 3. optimizer zero grad
+            optimizer.zero_grad()
+
+            # 4.loss backwords
+            loss.backward()
+
+            # 5.optimizer step
+            optimizer.step()
+
+            # 6.testing
+            self.model_2.eval()
+
+            with torch.inference_mode():
+                test_pred=self.model_2(self.y_test)
+                test_loss=loss_fn(self.y_test,test_pred)
+
+                    # Print out what's happening
+            if epoch % 100 == 0: 
+                print(f"Epoch: {epoch} | Train loss: {loss:.5f}, Test loss: {test_loss:.5f}")
+            
+            self.model_2.eval()
+            with torch.inference_mode():
+                self.y_pred = self.model_2(self.x_test)
+            
+
+class CircleModelV2(nn.Module):
+    def __init__(self,data:Data) -> None:
+        super().__init__()
+        self.d=data
+        self.layer1=nn.Linear(in_features=2,out_features=10)
+        self.layer2=nn.Linear(in_features=10, out_features=10)
+        self.layer3=nn.Linear(in_features=10,out_features=1)
+        #activation function wasn't included before 
+        self.relu=nn.ReLU()
+    def forward(self,x):
+        return(self.layer3(self.relu(self.layer2(self.relu(self.layer1(x))))))
+    
+    def accuracy_fn(self, y_true, y_pred):
+        corret=torch.eq(y_true,y_pred).sum().item() # torch.eq() calculates where two tensors are equa
+        return(corret/len(y_pred))*100
+    
+    def TrainModel(self)->None:
+        loss_fn=nn.BCEWithLogitsLoss()
+        optimizer=torch.optim.SGD(self.parameters(), lr=0.1)
+
+        epochs=1000
+        for epoch in range(epochs):
+            self.train()
+
+            y_logits=self(d.x_trian).squeeze()
+            y_pred=torch.round(torch.sigmoid(y_logits))
+
+            loss=loss_fn(y_logits,d.y_train)
+            acc=self.accuracy_fn(d.y_train,y_pred)
+
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            optimizer.step()
+
+            self.eval()
+
+            with torch.inference_mode():
+                test_logits=self(d.x_test).squeeze()
+                test_pred=torch.round(torch.sigmoid(test_logits))
+
+                test_loss=loss_fn(test_pred,d.y_test)
+                test_acc=self.accuracy_fn(d.y_test,test_pred)
+                if epoch % 100 == 0:
+                    print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Accuracy: {test_acc:.2f}%")
 
 d=Data()
 p=PlotStuff(d)
@@ -233,5 +342,10 @@ model1.ToTrain()
 p.PlotModel(model1)
 #despite the increase of hidden units it's still single line 
 
+Singleline=SingleLine()
+Singleline.model()
+# p.PlotLine(Singleline)
+# p.PlotLine(Singleline,Singleline.y_pred)
 
-
+model2=CircleModelV2(d)
+model2.TrainModel()
