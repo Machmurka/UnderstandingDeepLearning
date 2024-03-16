6fc7402c2c49813e6a58b3004fa09f311778e8db
jakmachura <jakmachura@gmail.com>
2024-03-16 13:51:32 +0100

    Fix bug in login functionality

---
+++ Learn PyTorch for Deep Learning/03_Pytorch_computer_Vision.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+import matplotlib as plt
+
+print(f"PyTorch version: {torch.__version__}\ntorchvision version: {torchvision.__version__}")
