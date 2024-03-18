0ace1770a1c479ef8eb3d82b15f3b1c2f11731db
jakmachura <jakmachura@gmail.com>
2024-03-18 22:52:50 +0100

    Add custom dataset class and test class

--- Learn PyTorch for Deep Learning/04_Pytorch_Custom_Datasets.py
+++ Learn PyTorch for Deep Learning/04_Pytorch_Custom_Datasets.py
@@ -11,7 +11,16 @@ import matplotlib.pylab as plt
 from torch.utils.data import  DataLoader
 from torchvision import datasets, transforms
 
-class SetData():
+import os
+import pathlib
+import torch
+
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+from typing import Tuple, Dict, List
+
+class Food101Data():
     def __init__(self) -> None:
       
         # Setup train and testing paths
@@ -41,8 +50,6 @@ class SetData():
       """
       for dirpath, dirnames, filenames in os.walk(dir_path):
         print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
-
-
 
     def transformData(self):
 
@@ -100,9 +107,102 @@ class SetData():
         img,label=next(iter(self.test_dataloader))
         print(f"shape of img in dataloader: {img.shape}-> [batch_size, color_channels, height, width]")
 
+
+# 1. Subclass torch.utils.data.Dataset
+class ImageFolderCustom(Dataset):
     
+    # 2. Initialize with a targ_dir and transform (optional) parameter
+    def __init__(self, targ_dir: str, transform=None) -> None:
+        
+        # 3. Create class attributes
+        # Get all image paths
+        self.paths = list(pathlib.Path(targ_dir).glob("*/*.jpg")) # note: you'd have to update this if you've got .png's or .jpeg's
+        # Setup transforms
+        self.transform = transform
+        # Create classes and class_to_idx attributes
+        self.classes, self.class_to_idx = self.find_classes(targ_dir)
+
+    # 4. Make function to load images
+    def load_image(self, index: int) -> Image.Image:
+        "Opens an image via a path and returns it."
+        image_path = self.paths[index]
+        return Image.open(image_path) 
+    
+    # 5. Overwrite the __len__() method (optional but recommended for subclasses of torch.utils.data.Dataset)
+    def __len__(self) -> int:
+        "Returns the total number of samples."
+        return len(self.paths)
+    
+    # 6. Overwrite the __getitem__() method (required for subclasses of torch.utils.data.Dataset)
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
+        "Returns one sample of data, data and label (X, y)."
+        img = self.load_image(index)
+        class_name  = self.paths[index].parent.name # expects path in data_folder/class_name/image.jpeg
+        class_idx = self.class_to_idx[class_name]
+
+        # Transform if necessary
+        if self.transform:
+            return self.transform(img), class_idx # return data, label (X, y)
+        else:
+            return img, class_idx # return data, label (X, y)
+        
+    def find_classes(self,directory:str)->Tuple[list[str],dict[str,int]]:
+        classes=sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+
+        if not classes:
+            raise FileNotFoundError(f"Couldn't find any classes in {directory}.")
+        
+        class_to_idx={cls_name: i for i, cls_name in enumerate(classes)}
+        
+        print(classes,class_to_idx)
+        return classes,class_to_idx
+
+class CustomDataTest():
+    def __init__(self) -> None:
+        """
+            temp placement for of transform
+        """
+        train_transform=transforms.Compose([
+            transforms.Resize(size=(64,64)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor()
+        ])
+    
+        test_transforms = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor()
+        ])
+    
+        train_data_custom =ImageFolderCustom(data.train_dir,transform=train_transform)
+        test_data_custom=ImageFolderCustom(data.test_dir,transform=test_transforms)
+    
+        """temp place for testing data"""    
+        # Check for equality amongst our custom Dataset and ImageFolder Dataset
+        print((len(train_data_custom) == len(data.train_data)) & (len(test_data_custom) == len(data.test_data)))
+        print(train_data_custom.classes == data.train_data.classes)
+        print(train_data_custom.class_to_idx == data.train_data.class_to_idx)
+
+   
+    def find_classes(self,directory:str)->Tuple[list[str],dict[str,int]]:
+        classes=sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+
+        if not classes:
+            raise FileNotFoundError(f"Couldn't find any classes in {directory}.")
+        
+        class_to_idx={cls_name: i for i, cls_name in enumerate(classes)}
+        
+        print(classes,class_to_idx)
+        return classes,class_to_idx
+    
+
+
+
 if __name__=="__main__":
-    data=SetData()
+    data=Food101Data()
     # data.ShowImg()
     data.transformData()
     data.intoDataLoader()
+
+    data1=CustomDataTest()
+    # data1.find_classes(data.train_dir)
+    
